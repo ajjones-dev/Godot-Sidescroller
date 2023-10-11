3af2883763790267199c3274a6027b06f8402e25
ajjones.dev <ajjones.dev@gmail.com>
2023-10-11 14:00:03 -0500

    Update unit movemement to basic path TODO: Add back gravity and chase mechanics

--- characters/unit.gd
+++ characters/unit.gd
@@ -1,34 +1,36 @@
 extends CharacterBody2D
 class_name Unit
 
-@export var speed : float = 100.0
+##Enemy Stats
+@export var speed : float = 30.0
 @onready var anim_player = get_node("AnimatedSprite2D")
-@onready var starting_position : Vector2 = self.global_position
-var wander_position : Vector2 = Vector2()
+var current_animation : String = "default"
+
+##Sprite Travel and Location Data
+var starting_position : Vector2 = Vector2()
+@export var move_direction: Vector2 = Vector2()
 var target_position : Vector2 = Vector2()
-@export var travel_distance : float = 4.0
-@export var chase_distance : float = 10.0
-var target : CharacterBody2D
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	
-	var direction = global_position.direction_to(target_position)
-	velocity = direction * speed
-	
-	move_and_slide()
+## Sets initial global position + defined
+func _ready():
+	starting_position = global_position
+	target_position = starting_position + move_direction
 
 func _process(delta):
-	_target_check()
-
-func _target_check():
-	if target != null:
-		var distance_from_target = global_position.distance_to(target.global_position)
-		if distance_from_target <= chase_distance:
-			target_position = target.global_position
+	global_position = global_position.move_toward(target_position, speed * delta)
+	var next_animation = "walk"
+	if global_position == target_position:
+		anim_player.flip_h = !anim_player.flip_h
+		if global_position == starting_position:
+			target_position = starting_position + move_direction
 		else:
-			target_position = wander_position
+			target_position = starting_position
+	animation_handler(next_animation)
+
+func animation_handler(animation_to_play : String):
+	if current_animation == animation_to_play:
+		return
+	else:
+		anim_player.stop()
+		anim_player.play(animation_to_play)
+		current_animation = animation_to_play
