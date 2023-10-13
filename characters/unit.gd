dea3bf07c33bd729ee00ede322287e7988ef6dc2
ajjones.dev <ajjones.dev@gmail.com>
2023-10-12 19:30:44 -0500

    Update robot from _process to _physics_process TODO: Figure out is_on_floor

--- characters/unit.gd
+++ characters/unit.gd
@@ -5,6 +5,7 @@ class_name Unit
 @export var speed : float = 30.0
 @onready var anim_player = get_node("AnimatedSprite2D")
 var current_animation : String = "default"
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 ##Sprite Travel and Location Data
 var starting_position : Vector2 = Vector2()
@@ -16,17 +17,21 @@ func _ready():
 	starting_position = global_position
 	target_position = starting_position + move_direction
 
-func _process(delta):
-	global_position = global_position.move_toward(target_position, speed * delta)
+func _physics_process(delta):
+	## Alternates between starting position and Vector2 coordinate as destination
+	velocity = position.direction_to(target_position) * speed
 	var next_animation = "walk"
-	if global_position == target_position:
+	if position.distance_to(target_position) <= 1:
 		anim_player.flip_h = !anim_player.flip_h
-		if global_position == starting_position:
+		if position.distance_to(starting_position) <= 1:
 			target_position = starting_position + move_direction
 		else:
 			target_position = starting_position
+	move_and_slide()
 	animation_handler(next_animation)
 
+## Changes animation based off string
+## TODO: Create Animation State Machine?
 func animation_handler(animation_to_play : String):
 	if current_animation == animation_to_play:
 		return
