731b6f3cd9b359793d83a5764c40835450192661
ajjones.dev <ajjones.dev@gmail.com>
2023-10-23 22:28:33 -0500

    Added Jump State for player

--- src/states/player_jump.gd
+++ src/states/player_jump.gd
@@ -4,13 +4,12 @@ class_name PlayerJump
 @export var parent : CharacterBody2D
 
 func enter():
-	super()
-
-func exit():
+	parent.velocity.y -= parent.JUMP_VELOCITY
 	super()
 
 func update_physics(delta : float):
-	pass
+	parent.move_and_slide()
+	ChangeState.emit(self, "fall")
 
 func update_event(event : InputEvent):
 	pass
