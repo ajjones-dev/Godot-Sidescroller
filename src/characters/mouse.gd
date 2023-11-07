5f902a0693ad6f2eb59c6e3679f6f9f9d828cfa0
ajjones.dev <ajjones.dev@gmail.com>
2023-11-07 11:34:42 -0600

    Added damage and weakspot to mouse

--- src/characters/mouse.gd
+++ src/characters/mouse.gd
@@ -7,14 +7,19 @@ var starting_position : Vector2 = Vector2()
 @export var move_direction: Vector2 = Vector2()
 var target_position : Vector2 = Vector2()
 var not_on_wall : bool = true
+@onready var hurt_vector : RayCast2D = get_node("HurtVector")
+var value : int = 5
 
 func _ready():
 	state_machine.init(self)
 
 func _physics_process(delta):
+	if hurt_vector.is_colliding() and hurt_vector.get_collider().has_method("take_damage"):
+		hurt_vector.get_collider().take_damage()
 	state_machine.update_physics(delta)
 
-func die():
+func die(body):
+	body.add_score(value)
 	queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
@@ -23,3 +28,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 func create(spawned_direction : Vector2) -> void:
 	move_direction = spawned_direction
 	starting_position = global_position
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		die(body)
