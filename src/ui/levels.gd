3426f5550ecde4a1126839395b9dfd7850eaff16
ajjones.dev <ajjones.dev@gmail.com>
2023-11-13 11:41:44 -0600

    Add missing elements to Level 2 for minimum play

--- src/ui/levels.gd
+++ src/ui/levels.gd
@@ -3,7 +3,7 @@ extends Node
 var levels = [
 	"res://ui/main_menu.tscn",
 	"res://levels/level1.tscn",
-	"res://levels/level1.tscn",
+	"res://levels/level2.tscn",
 	"res://levels/level1.tscn",
 	"res://ui/credits.tscn",
 	]
