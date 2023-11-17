5107c98ea81253ad3c58685ce0e2d7ac77c0fc57
ajjones.dev <ajjones.dev@gmail.com>
2023-11-17 17:01:24 -0600

    Added friction, sometimes jank

--- src/states/player_slide.gd
+++ src/states/player_slide.gd
@@ -2,13 +2,14 @@ extends State
 class_name PlayerSlide
 
 @export var parent : CharacterBody2D
-var friction : float = 0.1
+var friction : float = 0.5
 
 func enter():
 	if parent.is_on_floor():
 		parent.jump_count = 0
 		parent.has_double_jumped = false
-	#parent.hitbox.scale.y = 0.5
+	if parent.velocity.x < 0:
+		friction *= -1.0
 	super()
 
 func exit():
@@ -20,22 +21,17 @@ func update_physics(delta : float):
 		parent.jump_count += 1
 		ChangeState.emit(self, "fall")
 
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		parent.velocity.x = direction * parent.SPEED
+	parent.velocity.x -= parent.SPEED * friction * delta
 	parent.velocity.y = gravity * 2.0
 	
-	if direction < 0:
-		animation_player.flip_h = true
-	elif direction > 0:
-		animation_player.flip_h = false
-		
 	if parent.velocity.x == 0:
 		ChangeState.emit(self, "idle")
-	
-#	if Input.is_action_just_released("left") or Input.is_action_just_released("right"):
-#		if Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right"):
-#			ChangeState.emit(self, "walk")
+
+	if not Input.is_action_pressed("down"):
+		if (Input.is_action_pressed("left") or Input.is_action_pressed("right")):
+			ChangeState.emit(self, "walk")
+		else:
+			ChangeState.emit(self, "idle")
 	
 	parent.move_and_slide()
 
