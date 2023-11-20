d4be5305c118023d060908a7ddd31b7d12c085c6
ajjones.dev <ajjones.dev@gmail.com>
2023-11-20 17:14:45 -0600

    Fix slide friction, added downhill speed up TODO: Need to re-fix uphill speed

--- src/states/player_slide.gd
+++ src/states/player_slide.gd
@@ -8,8 +8,6 @@ func enter():
 	if parent.is_on_floor():
 		parent.jump_count = 0
 		parent.has_double_jumped = false
-	if parent.velocity.x < 0:
-		friction *= -1.0
 	super()
 
 func exit():
@@ -21,8 +19,8 @@ func update_physics(delta : float):
 		parent.jump_count += 1
 		ChangeState.emit(self, "fall")
 
-	parent.velocity.x -= parent.SPEED * friction * delta
-	parent.velocity.y = gravity * 2.0
+	parent.velocity.x -= parent.velocity.x * friction * delta
+	parent.velocity.y = gravity * 0.7
 	
 	if parent.velocity.x == 0:
 		ChangeState.emit(self, "idle")
