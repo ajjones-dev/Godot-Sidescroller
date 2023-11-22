f2202c6b32228ad77b952ffdecfc4bd394cf2789
ajjones.dev <ajjones.dev@gmail.com>
2023-11-22 15:23:33 -0600

    Start level3

--- src/ui/levels.gd
+++ src/ui/levels.gd
@@ -4,6 +4,6 @@ var levels = [
 	"res://ui/main_menu.tscn",
 	"res://levels/level1.tscn",
 	"res://levels/level2.tscn",
-	"res://levels/level1.tscn",
+	"res://levels/level3.tscn",
 	"res://ui/credits.tscn",
 	]
