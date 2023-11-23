cd8a8a2a9b336f648deb0bf0d1972a7bf3807469
ajjones.dev <ajjones.dev@gmail.com>
2023-11-23 11:03:37 -0600

    Add framework for fly enemy

---
+++ src/states/enemy_fly.gd
@@ -0,0 +1,17 @@
+extends State
+class_name EnemyFly
+
+func enter():
+	super()
+
+func exit():
+	super()
+
+func update_event(_event):
+	pass
+
+func update_process(_delta):
+	pass
+
+func update_physics(_delta):
+	pass
