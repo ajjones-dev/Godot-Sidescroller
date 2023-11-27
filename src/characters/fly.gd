53948cd09665d82ce9553cc7feb74e3e94d91ae6
ajjones.dev <ajjones.dev@gmail.com>
2023-11-27 11:43:15 -0600

    Add fly spawners to level3

--- src/characters/fly.gd
+++ src/characters/fly.gd
@@ -49,7 +49,7 @@ func create(spawned_direction : Vector2) -> void:
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-
+## Player enters Weakspot to destroy enemy
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		die(body)
